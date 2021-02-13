d4fa6fd87b01fdfddbb2334bb0b557a18849ec9b
MichaelDev2911 <michaelsilveira.dev@outlook.com>
2021-02-13 18:44:23 -0300

    Boas praticas construtor Filme

--- exercicio.1/src/Aplicacao.java
+++ exercicio.1/src/Aplicacao.java
@@ -10,9 +10,12 @@ public class Aplicacao {
                 120, 2014, 5, diretor);
         Filme filme2 = new Filme("Dragon Ball", "Melhor dezenho",
                 30, 1987, 5, diretor);
-
+        Filme filme3 = new Filme("Batman vs SuperMan","Show de horror",
+                1000,2010,5,diretor);
+        
         filme1.reproduzir();
         filme2.reproduzir();
+        filme3.reproduzir();
         diretor.imprimir();
         ator.imprimir();
     }
--- exercicio.1/src/Filme.java
+++ exercicio.1/src/Filme.java
@@ -8,11 +8,14 @@ public class Filme {
     private Diretor diretor;
 
     public Filme(String nome, String descricao, Integer duracao, Integer anoLancamento, Integer avaliacao, Diretor diretor) {
+        defineAvaliacao(avaliacao);
+        validaNomeEDefineAvaliacao(nome);
+
+
         this.nome = nome;
         this.descricao = descricao;
         this.duracao = duracao;
         this.anoLancamento = anoLancamento;
-        this.avaliacao = avaliacao;
         this.diretor = diretor;
     }
 
@@ -21,6 +24,24 @@ public class Filme {
         System.out.println("Descrição: " + this.descricao);
         System.out.println("Duração: " + this.duracao);
         System.out.println("Nome do Diretor: " + this.diretor.getNome());
+        System.out.println("Avaliação:" + this.avaliacao);
         System.out.println("-------------------");
     }
+    private void defineAvaliacao(Integer avaliacao){
+        if(avaliacao < 1 || avaliacao > 5){
+            this.avaliacao = 3;
+        }else{
+            this.avaliacao =avaliacao;
+        }
+    }
+    private void validaNomeEDefineAvaliacao(String nome){
+        if("Batman vs SuperMan".equals(nome)){
+            this.avaliacao=1;
+        }else if("O clube da luta".equals(nome)){
+            this.avaliacao= 5;
+        }else{
+            this.avaliacao=avaliacao;
+        }
+    }
+
 }
